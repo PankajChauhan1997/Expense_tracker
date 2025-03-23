82dce21b50d2d910d06a627d82edfc9f1f7fc0ab
Pankaj.Chauhan <Pankaj.Chauhan@greytrixiindia.com>
2025-03-23 16:52:58 +0530

    expense_tracker_completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,18 @@
 import 'package:flutter/material.dart';
-
+import 'package:flutter/services.dart';
 import 'widgets/expenses_List/expenses.dart';
 
 var kcolorScheme=ColorScheme.fromSeed(seedColor: Color.fromARGB(255,96,59,181));
 var kDarkColorScheme=ColorScheme.fromSeed(brightness:Brightness.dark,seedColor: Color.fromARGB(255,5,99,125));
 void main() {
-  runApp(const MyApp());
+  // WidgetsFlutterBinding.ensureInitialized();
+  // SystemChrome.setPreferredOrientations([///app will run only on portrait mode
+  //   DeviceOrientation.portraitUp,
+  // ]).then((fn){
+    runApp(const MyApp());
+  // });
+
+
 }
 
 class MyApp extends StatelessWidget {
--- lib/widgets/expenses_List/expenses.dart
+++ lib/widgets/expenses_List/expenses.dart
@@ -70,105 +70,110 @@ class _ExpensesState extends State<Expenses> {
     selectedDate = null;
   }
   void _addExpenses() {
+    final keyboardSpace=MediaQuery.of(context).viewInsets.bottom;
     showModalBottomSheet(
       context: context,
+      useSafeArea : true,
       isScrollControlled: true,
       builder: (BuildContext context) {
         Category modalCategory = selectedCategory;
         DateTime? modalSelectedDate = selectedDate; // Local state for modal
         return StatefulBuilder(
             builder: (BuildContext modalContext, StateSetter setModalState) {
-              return Container(
-                height: MediaQuery.of(context).size.height, // Full screen height
-                child: Padding(
-                  padding: const EdgeInsets.fromLTRB(16.0, 48, 16, 16),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      TextField(
-                        controller: expTitle,
-                        maxLength: 50,
-                        decoration: const InputDecoration(labelText: "Title"),
-                      ),
-                      Row(
-                        children: [
-                          Flexible(
-                            child: TextField(
-                              controller: expAmt,
-                              keyboardType: TextInputType.number,
-                              decoration: const InputDecoration(
-                                  labelText: "Amount", prefixText: "₹"),
-                            ),
-                          ),
-                          const SizedBox(width: 16),
-                          Flexible(
-                            child: InkWell(
-                              onTap: () async {
-                                final pickedDate = await showDatePicker(
-                                  context: modalContext,
-                                  initialDate: DateTime.now(),
-                                  firstDate: DateTime(1997, DateTime.now().month, DateTime.now().day),
-                                  lastDate: DateTime.now(),
-                                );
-                                if (pickedDate != null) {
-                                  setModalState(() {
-                                    modalSelectedDate = pickedDate;
-                                  });
-                                  // Update the main state's selectedDate
-                                  setState(() {
-                                    selectedDate = pickedDate;
-                                  });
-                                }
-                              },
-                              child: Row(
-                                crossAxisAlignment: CrossAxisAlignment.center,
-                                children: [
-                                  Padding(
-                                    padding: const EdgeInsets.only(right: 20.0),
-                                    child: Text(
-                                        '${modalSelectedDate != null
-                                            ? formatter.format(modalSelectedDate!)
-                                            : "Select Date"}'),
-                                  ),
-                                  const Icon(Icons.calendar_month),
-                                ],
+              return SizedBox(
+                height: double.infinity,//MediaQuery.of(context).size.height, // Full screen height
+                width: double.infinity,//MediaQuery.of(context).size.height, // Full screen height
+                child: SingleChildScrollView(
+                  child: Padding(
+                    padding:  EdgeInsets.fromLTRB(16.0, 48, 16,keyboardSpace+16,),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        TextField(
+                          controller: expTitle,
+                          maxLength: 50,
+                          decoration: const InputDecoration(labelText: "Title"),
+                        ),
+                        Row(
+                          children: [
+                            Flexible(
+                              child: TextField(
+                                controller: expAmt,
+                                keyboardType: TextInputType.number,
+                                decoration: const InputDecoration(
+                                    labelText: "Amount", prefixText: "₹"),
                               ),
                             ),
-                          ),
-                        ],
-                      ),
-                      DropdownButton<Category>(
-                        value: modalCategory, // Use modal state
-                        onChanged: (Category? newValue) {
-                          if (newValue != null) {
-                            setModalState(() {
-                              modalCategory = newValue; // Update inside modal state
-                            });
-                            // Update the main state's selectedCategory
-                            setState(() {
-                              selectedCategory = newValue;
-                            });
-                            print("Selected in modal: $modalCategory");
-                          }
-                        },
-                        items: Category.values.map((category) {
-                          return DropdownMenuItem(
-                            value: category,
-                            child: Text(category.name.toUpperCase()),
-                          );
-                        }).toList(),
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceAround,
-                        children: [
-                          ElevatedButton(
-                              onPressed: submitExpense, child: const Text('Save')),
-                          ElevatedButton(onPressed: () =>
-                              Navigator.of(context).pop(), child: const Text('Cancel')),
-                        ],
-                      )
-                    ],
+                            const SizedBox(width: 16),
+                            Flexible(
+                              child: InkWell(
+                                onTap: () async {
+                                  final pickedDate = await showDatePicker(
+                                    context: modalContext,
+                                    initialDate: DateTime.now(),
+                                    firstDate: DateTime(1997, DateTime.now().month, DateTime.now().day),
+                                    lastDate: DateTime.now(),
+                                  );
+                                  if (pickedDate != null) {
+                                    setModalState(() {
+                                      modalSelectedDate = pickedDate;
+                                    });
+                                    // Update the main state's selectedDate
+                                    setState(() {
+                                      selectedDate = pickedDate;
+                                    });
+                                  }
+                                },
+                                child: Row(
+                                  crossAxisAlignment: CrossAxisAlignment.center,
+                                  children: [
+                                    Padding(
+                                      padding: const EdgeInsets.only(right: 20.0),
+                                      child: Text(
+                                          '${modalSelectedDate != null
+                                              ? formatter.format(modalSelectedDate!)
+                                              : "Select Date"}'),
+                                    ),
+                                    const Icon(Icons.calendar_month),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                        DropdownButton<Category>(
+                          value: modalCategory, // Use modal state
+                          onChanged: (Category? newValue) {
+                            if (newValue != null) {
+                              setModalState(() {
+                                modalCategory = newValue; // Update inside modal state
+                              });
+                              // Update the main state's selectedCategory
+                              setState(() {
+                                selectedCategory = newValue;
+                              });
+                              print("Selected in modal: $modalCategory");
+                            }
+                          },
+                          items: Category.values.map((category) {
+                            return DropdownMenuItem(
+                              value: category,
+                              child: Text(category.name.toUpperCase()),
+                            );
+                          }).toList(),
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: [
+                            ElevatedButton(
+                                onPressed: submitExpense, child: const Text('Save')),
+                            ElevatedButton(onPressed: () =>
+                                Navigator.of(context).pop(), child: const Text('Cancel')),
+                          ],
+                        )
+                      ],
+                    ),
                   ),
                 ),
               );
@@ -193,6 +198,7 @@ class _ExpensesState extends State<Expenses> {
 
   @override
   Widget build(BuildContext context) {
+    final width=MediaQuery.of(context).size.width;
     return Scaffold(
       appBar: AppBar(
         title: const Text("Expense Tracker"),
@@ -205,21 +211,19 @@ class _ExpensesState extends State<Expenses> {
                     style: TextStyle(fontSize: 16),
                   ),
           )
-          :Center(
-        child:
-        // Column(
-        //   crossAxisAlignment: CrossAxisAlignment.center,
-        //   mainAxisAlignment: MainAxisAlignment.center,
-        //   children: [
-        //     Expanded(child: ExpenseList(expenses: _registeredExpenses, onremove: _removeExpenses)),
-        //   ],
-        // ),
+          :Center(child:
+      width < 600?
         Column(
           children: [
             Chart(expenses: _registeredExpenses),
             Expanded(child: ExpenseList(expenses: _registeredExpenses, onremove: _removeExpenses)),
           ],
-        ),
+        ): Row(
+        children: [
+          Expanded(child:Chart(expenses: _registeredExpenses)),
+          Expanded(child: ExpenseList(expenses: _registeredExpenses, onremove: _removeExpenses)),
+        ],
+      ),
       ),
     );
   }
